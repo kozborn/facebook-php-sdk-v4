668da746b936788675a55a431e90dbe5af8c2289
Piotrek-Brutus <piotrkozubek@gmail.com>
2014-06-16 21:55:40 +0200

    FacebookRedirectLoginHandler prepared for PHP v5.3

--- src/Facebook/FacebookRedirectLoginHelper.php
+++ src/Facebook/FacebookRedirectLoginHelper.php
@@ -138,12 +138,13 @@ class FacebookRedirectLoginHelper
           FacebookSession::_getTargetAppSecret($this->appSecret),
         'code' => $this->getCode()
       );
-      $response = (new FacebookRequest(
+      $fbRequest = new FacebookRequest(
         FacebookSession::newAppSession($this->appId, $this->appSecret),
         'GET',
         '/oauth/access_token',
         $params
-      ))->execute()->getResponse();
+      );
+      $response = $fbRequest->execute()->getResponse();
       if (isset($response['access_token'])) {
         return new FacebookSession($response['access_token']);
       }
